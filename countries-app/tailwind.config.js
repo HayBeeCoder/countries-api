475eb125c209e1042d849674863a49c49237c0d6
HayBeeCoder <54000678+HayBeeCoder@users.noreply.github.com>
2021-12-04 01:17:07 +0100

    updated npm package

--- countries-app/tailwind.config.js
+++ countries-app/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.js', './public/index.html'],
+  purge: ['./src/**/*.js','./public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
